e6a208b22ac01f6d0df5c1d1dae84c88bd441382
qafarzadee <elgunag@code.edu.az>
2023-09-21 00:12:59 +0400

    all done

---
+++ Ecommerce/Web.UI/Controllers/payment.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Web.UI.Controllers
+{
+    public class payment : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
